d9c76d1d3d647a40cbb72a16e99e9b9d20baba11
dawids12 <101203650+dawids12@users.noreply.github.com>
2022-05-16 17:33:18 +0000

    commit3.1

--- Main.java
+++ Main.java
@@ -1,15 +1,39 @@
+import java.util.Scanner;
 import java.io.IOException;
 
 class Main {
   public static void main(String[] args) {
     try {
       Service1 s = new Service1();
-      s.addStudent(new Student("Krzysztof", 20));
-      s.addStudent(new Student("Janusz", 40));
+      Scanner scan = new Scanner(System.in);
+      int opcja = 10;
+      String imie;
+      int wiek;
+      System.out.println("MENU");
+      System.out.println("1 - Dodawanie studenta");
+      System.out.println("2 - Wyświetlenie listy studentów");
+      System.out.println("0 - wyjście z programu");
+      while(opcja != 0){
+        System.out.println("Podaj opcje:");
+        opcja = scan.nextInt();
+        scan.nextLine();
+        switch(opcja){
+          case 1:
+            System.out.println("Podaj imie: ");
+            imie = scan.nextLine();
+            System.out.println("Podaj wiek:");
+            wiek = scan.nextInt();
+            scan.nextLine();
+            s.addStudent(new Student(imie, wiek));
 
-      var students = s.getStudents();
-      for(Student current : students) {
-        System.out.println(current.ToString());
+          case 2:
+            var students = s.getStudents();
+            for(Student current : students) {
+            System.out.println(current.ToString());
+        }
+      }
+        
+      
       }
     } catch (IOException e) {
 
